cd8832217bd8741bfb636186295947ff1feb3eb2
Igor Oryński <igor.orynski@gmail.com>
2024-05-22 22:12:28 +0200

    generate new tiles on player move

--- Assets/Scripts/BackgroundGenerator.cs
+++ Assets/Scripts/BackgroundGenerator.cs
@@ -1,15 +1,21 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 namespace Survivors
 {
+    [Serializable]
+    public class RandTile
+    {
+        public Tile Tile;
+        public float Weight;
+    }
+
     [RequireComponent(typeof(Tilemap))]
     public class BackgroundGenerator : MonoBehaviour
     {
+        public int MapMargin = 5;
         public RandTile[] Tiles;
 
         private Tilemap tilemap;
@@ -22,15 +28,23 @@ namespace Survivors
             tilemap = GetComponent<Tilemap>();
             weightSum = Tiles.Select(val => val.Weight).Sum();
 
-            for (int x = -5; x < 5; x++)
-                for (int y = -5; y < 5; y++)
+            for (int x = -MapMargin; x < MapMargin; x++)
+                for (int y = -MapMargin; y < MapMargin; y++)
                     if (TryGetRandomTile(out var tile))
                         tilemap.SetTile(new Vector3Int(x, y, 0), tile);
         }
 
         private void FixedUpdate()
-        {            
+        {
+            var playerPos = GameManager.PlayerInstance.transform.position;
+            var bounds_horizontal = new Vector2Int(Mathf.FloorToInt(playerPos.x) - MapMargin, Mathf.CeilToInt(playerPos.x) + MapMargin);
+            var bounds_vertical = new Vector2Int(Mathf.FloorToInt(playerPos.y) - MapMargin, Mathf.CeilToInt(playerPos.y) + MapMargin);
 
+            for (int x = bounds_horizontal.x; x < bounds_horizontal.y; x++)
+                for (int y = bounds_vertical.x; y < bounds_vertical.y; y++)
+                    if (tilemap.GetTile(new Vector3Int(x, y, 0)) == null)
+                        if (TryGetRandomTile(out var tile))
+                            tilemap.SetTile(new Vector3Int(x, y, 0), tile);
         }
 
         private Tile GetRandomTile()
@@ -52,12 +66,5 @@ namespace Survivors
             tile = Tiles.First(val => (rand -= val.Weight) < 0).Tile;
             return true;
         }
-    }
-
-    [Serializable]
-    public class RandTile
-    {
-        public Tile Tile;
-        public float Weight;
     }
 }
