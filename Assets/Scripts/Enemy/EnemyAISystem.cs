b89abbdfc8f7634023cfbc96fc30bdd13e1c3468
Igor Oryński <igor.orynski@gmail.com>
2024-06-08 15:58:46 +0200

    jobify enemy movement

--- Assets/Scripts/Enemy/EnemyAISystem.cs
+++ Assets/Scripts/Enemy/EnemyAISystem.cs
@@ -1,23 +1,81 @@
+using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
+using Unity.Burst;
 
 namespace Survivors
 {
+    [BurstCompile]
     public partial struct EnemyAISystem : ISystem
     {
-        private EntityManager entityManager;
-        private Entity playerEntity;
-
+        [BurstCompile]
         private void OnUpdate(ref SystemState state)
         {
-            entityManager = state.EntityManager;
-            playerEntity = SystemAPI.GetSingletonEntity<PlayerComponent>();
+            var entityManager = state.EntityManager;
+            var playerEntity = SystemAPI.GetSingletonEntity<PlayerComponent>();
 
-            foreach (var (enemyComponent, transformComponent) in SystemAPI.Query<EnemyComponent, RefRW<LocalTransform>>())
+            var entitiesQuery = new EntityQueryBuilder(Allocator.TempJob)
+                .WithAll<EnemyComponent>()
+                .WithAll<LocalTransform>()
+                .Build(ref state);
+
+            var entitiesArray = entitiesQuery.ToEntityArray(Allocator.TempJob);
+
+            var buffers = new NativeArray<EntityCommandBuffer>(entitiesArray.Length, Allocator.TempJob);
+            for (int i = 0; i < entitiesArray.Length; i++)
+                buffers[i] = new EntityCommandBuffer(Allocator.TempJob);
+
+            var handles = new NativeArray<JobHandle>(entitiesArray.Length, Allocator.TempJob);
+            var enemyComponents = entitiesQuery.ToComponentDataArray<EnemyComponent>(Allocator.TempJob);
+            var transforms = entitiesQuery.ToComponentDataArray<LocalTransform>(Allocator.TempJob);
+
+            var playerPosition = entityManager.GetComponentData<LocalTransform>(playerEntity).Position;
+            var deltaTime = SystemAPI.Time.DeltaTime;
+
+            for (int i = 0; i < entitiesArray.Length; i++)
             {
-                var direction = entityManager.GetComponentData<LocalTransform>(playerEntity).Position - transformComponent.ValueRO.Position;
-                transformComponent.ValueRW.Position += enemyComponent.MoveSpeed * SystemAPI.Time.DeltaTime * math.normalize(direction);
+                var moveJob = new MoveJob
+                {
+                    Buffer = buffers[i],
+                    EnemyEntity = entitiesArray[i],
+                    PlayerPosition = playerPosition,
+                    EnemyComponent = enemyComponents[i],
+                    TransformComponent = transforms[i],
+                    DeltaTime = deltaTime
+                };
+                handles[i] = moveJob.Schedule();
+            }
+            JobHandle.CompleteAll(handles);
+
+            for (int i = 0; i < entitiesArray.Length; i++)
+            {
+                buffers[i].Playback(state.EntityManager);
+                buffers[i].Dispose();
+            }
+            buffers.Dispose();
+            entitiesArray.Dispose();
+            handles.Dispose();
+            enemyComponents.Dispose();
+            transforms.Dispose();
+        }
+
+        [BurstCompile]
+        private struct MoveJob : IJob
+        {
+            public EntityCommandBuffer Buffer;
+            public Entity EnemyEntity;
+            public float3 PlayerPosition;
+            public EnemyComponent EnemyComponent;
+            public LocalTransform TransformComponent;
+            public float DeltaTime;
+
+            public void Execute()
+            {
+                var direction = PlayerPosition - TransformComponent.Position;
+                TransformComponent.Position += EnemyComponent.MoveSpeed * DeltaTime * math.normalizesafe(direction);
+                Buffer.SetComponent(EnemyEntity, TransformComponent);
             }
         }
     }
