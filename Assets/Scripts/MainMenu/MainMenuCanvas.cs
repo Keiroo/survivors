07fb51c3c5b566b7ca9fd42b1442195d091d038f
Igor Oryński <igor.orynski@gmail.com>
2024-06-17 22:40:08 +0200

    import ui pack; add volume control

--- Assets/Scripts/MainMenu/MainMenuCanvas.cs
+++ Assets/Scripts/MainMenu/MainMenuCanvas.cs
@@ -1,6 +1,7 @@
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Survivors
 {
@@ -8,25 +9,34 @@ namespace Survivors
     {
         public RectTransform Title;
         public RectTransform Content;
+        public Slider VolumeSlider;
 
         private void Start()
         {
-            var sequence = DOTween.Sequence();
+            if (Title == null) return;
+            if (Content == null) return;
 
+            var sequence = DOTween.Sequence();
             Title.anchoredPosition = new Vector2(0f, 100f);
             Content.anchoredPosition = new Vector2(0f, -200f);
+            sequence.AppendInterval(1f);
+            sequence.Append(Title.DOAnchorPosY(0f, 1f).SetEase(Ease.OutCubic));
+            sequence.Insert(2.5f, Content.DOAnchorPosY(0f, 1f).SetEase(Ease.OutCubic));
 
-
-            sequence.Append(Title.DOAnchorPosY(0f, 1f).SetEase(Ease.OutElastic));
-            sequence.Insert(0.5f, Content.DOAnchorPosY(0f, 1f).SetEase(Ease.OutCubic));
-
-            
+            if (VolumeSlider == null) return;
+            VolumeSlider.value = GameManager.BackgroundMusicVolume;
+            VolumeSlider.onValueChanged.AddListener(ChangeVolume);
         }
 
         public void LoadMainGameScene()
         {
             SceneManager.LoadScene(1);
             SceneManager.LoadScene(2, LoadSceneMode.Additive);
+        }
+
+        private void ChangeVolume(float volume)
+        {
+            GameManager.BackgroundMusicVolume = volume;
         }
     }
 }
--- Assets/Scripts/Misc/GameManager.cs
+++ Assets/Scripts/Misc/GameManager.cs
@@ -5,5 +5,6 @@ namespace Survivors
     public static class GameManager
     {
         public static GameObject PlayerInstance;
+        public static float BackgroundMusicVolume = 0.5f;
     }
 }
